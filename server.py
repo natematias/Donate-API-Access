196616fd168dbff2b5afeae61ffb98d6bb0ceb03
Nathan Matias <natematias@gmail.com>
2017-05-02 18:10:47 -0400

    updated Kawan's code to work for latest versions of Tweepy. Added feature to output keys to file for research donation purposes

--- server.py
+++ server.py
@@ -4,76 +4,96 @@ flask-tweepy-oauth
 an example showing how to authorize a twitter application
 in python with flask and tweepy.
 
+Updated from Kawan's original script at:
 find me on github at github.com/whichlight
-see my other projects at whichlight.com
 
-KAWAN!
+J. Nathan Matias
+
+To set up, make a local copy of the config/env.json file such as development.json
+
+Then set CS_ENV=development to specify which environment to run in
+
 '''
 
 from flask import Flask
 from flask import request
+from flask import session
 import flask 
 import tweepy
+import simplejson as json
+import os
 app = Flask(__name__)
 
-#config
-	
-CONSUMER_TOKEN='fill this out'
-CONSUMER_SECRET='and this'
+ENV = os.environ['CS_ENV']
+
+config = json.loads(open(os.path.join("config",ENV+".json"),"r").read())
+
+app.secret_key = config['secret_key']
+
+CONSUMER_TOKEN=config['consumer_token']
+CONSUMER_SECRET=config['consumer_secret']
 CALLBACK_URL = 'http://localhost:5000/verify'
-session = dict()
-db = dict() #you can save these values to a database
+db = {}#dict() #you can save these values to a database
 
 @app.route("/")
 def send_token():
-	auth = tweepy.OAuthHandler(CONSUMER_TOKEN, 
-		CONSUMER_SECRET, 
-		CALLBACK_URL)
-	
-	try: 
-		#get the request tokens
-		redirect_url= auth.get_authorization_url()
-		session['request_token']= (auth.request_token.key,
-			auth.request_token.secret)
-	except tweepy.TweepError:
-		print 'Error! Failed to get request token'
-	
-	#this is twitter's url for authentication
-	return flask.redirect(redirect_url)	
+  auth = tweepy.OAuthHandler(CONSUMER_TOKEN, 
+    CONSUMER_SECRET, 
+    CALLBACK_URL)
+  
+  try: 
+    #get the request tokens
+    redirect_url= auth.get_authorization_url()
+    session['request_token']= (auth.request_token['oauth_token'],
+      auth.request_token['oauth_token_secret'])
+  except tweepy.TweepError:
+    print 'Error! Failed to get request token'
+  
+  #this is twitter's url for authentication
+  return flask.redirect(redirect_url)  
 
 @app.route("/verify")
 def get_verification():
-	
-	#get the verifier key from the request url
-	verifier= request.args['oauth_verifier']
-	
-	auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
-	token = session['request_token']
-	del session['request_token']
-	
-	auth.set_request_token(token[0], token[1])
+  
+  #get the verifier key from the request url
+  verifier= request.args['oauth_verifier']
+  
+  auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
+  token = session['request_token']
+  auth.request_token = {"oauth_token":session['request_token'][0],
+                        "oauth_token_secret": session['request_token'][1]}
+  del session['request_token']
+  #auth.set_request_token(token[0], token[1])
 
-	try:
-		    auth.get_access_token(verifier)
-	except tweepy.TweepError:
-		    print 'Error! Failed to get access token.'
-	
-	#now you have access!
-	api = tweepy.API(auth)
+  try:
+        auth.get_access_token(verifier)
+#  auth.get_access_token(verifier)
 
-	#store in a db
-	db['api']=api
-	db['access_token_key']=auth.access_token.key
-	db['access_token_secret']=auth.access_token.secret
-	return flask.redirect(flask.url_for('start'))
+  except tweepy.TweepError:
+        print 'Error! Failed to get access token.'
+  
+  #now you have access!
+  api = tweepy.API(auth)
+  username = api.auth.get_username()
+  
 
-@app.route("/start")
-def start():
-	#auth done, app logic can begin
-	api = db['api']
+  output_dict = {"username":username,
+                  "oauth_token":token[0],
+                  "oauth_token_secret":token[1]}
+  with open(os.path.join("keys",username+".json"), "w") as f:
+    f.write(json.dumps(output_dict))
 
-	#example, print your latest status posts
-	return flask.render_template('tweets.html', tweets=api.user_timeline())
+  session['username'] = username
+
+  ## ARCHIVE TO A NEW FILE
+  return flask.redirect(flask.url_for('success'))
+
+@app.route("/success")
+def success():
+  #auth done, app logic can begin
+  return flask.render_template('success.html', username=session['username'])
 
 if __name__ == "__main__":
-	app.run()
+  app.run(debug=True)
+  #app.run()
+
